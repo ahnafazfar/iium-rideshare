1db880f9399d7a793703ae05136874041a1db102
ahnafazfar <ahnafazfar04@gmail.com>
2026-01-07 18:17:16 +0800

    Initial commit

---
+++ database/migrations/2026_01_07_094859_add_extra_fields_to_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('matric_no')->unique();
+            $table->boolean('verified')->default(false);
+            $table->enum('role', ['passenger', 'admin'])->default('passenger');
+            $table->string('contact_link')->nullable();
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
